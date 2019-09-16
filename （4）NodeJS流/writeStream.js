07ed6b2a3aaa02632426c32d0173d379a14deb7b
Fankexin <974094944@qq.com>
2019-09-16 16:23:09 +0800

    .

--- （4）NodeJS流/writeStream.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-var filePath = path.join(__dirname,"/stream.txt");
-var writePath = path.join(__dirname,"/write.txt");
-var readStream = fs.createReadStream(filePath);
-var writeSream = fs.createWriteStream(writePath);
-readStream.pipe(writeSream);
